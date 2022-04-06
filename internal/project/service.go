dae578968148cd9e7a9262129af43c4f988f3649
rasulov-emirlan <rasulov.emirlan@gmail.com>
2022-04-06 19:06:40 +0600

    added new interfaces to the service

--- internal/project/service.go
+++ internal/project/service.go
@@ -2,6 +2,7 @@ package project
 
 import (
 	"context"
+	"errors"
 	"io"
 )
 
@@ -10,11 +11,32 @@ type Service interface {
 	Delete(ctx context.Context, id ID) error
 }
 
-type service struct {
+type FileSystem interface {
+	Upload(ctx context.Context, f io.Reader, id string) (path string, err error)
+	Delete(ctx context.Context, id string) error
 }
 
-func NewService() (Service, error) {
-	return &service{}, nil
+type Repository interface {
+	Create(ctx context.Context, p Project) (Project, error)
+	Read(ctx context.Context, id ID) (Project, error)
+
+	Update(ctx context.Context, id ID, p Project) (Project, error)
+	Delete(ctx context.Context, p Project) error
+}
+
+type service struct {
+	fs   FileSystem
+	repo Repository
+}
+
+func NewService(fs FileSystem, repo Repository) (Service, error) {
+	if fs == nil || repo == nil {
+		return nil, errors.New("project: arguments for creating new service can't be nil")
+	}
+	return &service{
+		fs:   fs,
+		repo: repo,
+	}, nil
 }
 
 func (s *service) Deploy(ctx context.Context, f io.Reader, name, basePath string, isSPA bool) (Project, error) {
