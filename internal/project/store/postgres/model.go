82971b9b505f2b696c8ca2c07c9649e639591797
rasulov-emirlan <rasulov.emirlan@gmail.com>
2022-04-07 13:11:10 +0600

    fixed gorm model

--- internal/project/store/postgres/model.go
+++ internal/project/store/postgres/model.go
@@ -4,7 +4,7 @@ type projectModel struct {
 	ID string `gorm:"type:uuid, primaryKey; column:id"`
 
 	Name     string `gorm:"column: project_name"`
-	BasePath string `gorm:"index; column:"base_path"`
+	BasePath string `gorm:"index; column:base_path"`
 	RealPath string `gorm:"real_path"`
 	IsSPA    bool   `gorm:"column:is_spa"`
 }
